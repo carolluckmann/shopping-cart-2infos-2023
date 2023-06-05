56a2ed0949a7458b229fd398128d0f9272f963f3
carolluckmann <carolinaluckmann05@gmail.com>
2023-06-05 16:44:27 -0300

    commit3

---
+++ src/components/ListagemLivros.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { livros } from '@/_data/livros.js'
+import { adicionarAoCarrinho } from '@/_data/carrinho.js'
+import cardLivro from '@/components/CardLivro.vue'
+</script>
+
+<template>
+  <div class="listagem-livros">
+    <card-livro 
+      v-for="livro in livros" 
+      v-bind:key="livro.id" 
+      v-bind:livro="livro" 
+      @adicionarAoCarrinho="adicionarAoCarrinho"
+    />
+  </div>
+</template>
+
+<style setup>
+.listagem-livros {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
